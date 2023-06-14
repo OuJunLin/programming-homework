d7349b0de7ae62ed26e074cafbbf085a3edb8324
OuJunLin <ouchoj2019go@gmail.com>
2023-06-14 15:05:21 +0800

    go

--- MySQLFun.py
+++ MySQLFun.py
@@ -57,6 +57,75 @@ def get_user_data():
 
 
 
+def insert_to_img_info(class_:str, file_name:str,time_, nick_name:str):
+    now_time = getTime()
+    cursor.execute('INSERT INTO `img_info`(class, file_name, time, Nick_name) VALUES ("{}", "{}", "{}", "{}")' .format(class_, file_name, time_, nick_name))
+    dbConn.commit()
+
+
+
+def get_img_info():
+    img_data_dict = {}
+    cursor.execute("SELECT * FROM `img_info`;")
+    for row in cursor.fetchall():
+        img_data_dict[row[4]] = [row[1], row[2], row[3]]
+    return img_data_dict
+
+
+
+def update_img_info_Nick_name(new_name:str, old_name:str):
+    check = get_img_info()
+    if old_name in check:
+        cursor.execute('UPDATE `img_info` SET Nick_name = "{}" WHERE Nick_name = "{}";' .format(new_name, old_name))
+        dbConn.commit()
+    else:
+        print("not in db")
+
+
+
+def delete_img_info(name:str):
+    cursor.execute('DELETE FROM `img_info` WHERE Nick_name = "{}";' .format(name))
+    dbConn.commit()
+
+
+
+def inser_to_chatroom_record(account:str, msg:str):
+    nowtime = getTime()
+    cursor.execute('INSERT INTO `chatroom_record`(account, msg, time) VALUES ("{}", "{}", "{}")' .format(account, msg, nowtime))
+    dbConn.commit()
+
+
+
+def insert_to_phone_record(phone:str):
+    cursor.execute('INSERT INTO `phone_record`(phone, time) VALUES ("{}", "{}")' .format(phone, getTime()))
+    dbConn.commit()
+
+
+
+def update_account_password(newpassword:str, account:str):
+    cursor.execute('UPDATE `account_password` SET password = "{}" WHERE account = "{}"'.format(newpassword, account))
+    dbConn.commit()
+
+
+
+def update_user_data(set_choose:str, set_content:str, user_name:str):
+    cursor.execute('UPDATE `user_data` SET {} = "{}" WHERE account = "{}"'.format(set_choose, set_content, user_name))
+    dbConn.commit()
+
+
+
+def delete_user_data_account(account:str):
+    cursor.execute('DELETE FROM `user_data` WHERE account = "{}";' .format(account))
+    dbConn.commit()
+
+
+
+def delete_account_password_account(account:str):
+    cursor.execute('DELETE FROM `account_password` WHERE account = "{}";' .format(account))
+    dbConn.commit()
+
+
+
 
 
 
--- connectFun.py
+++ connectFun.py
@@ -4,13 +4,32 @@ import mysql.connector as myconn
 import datetime
 import MySQLFun
 import time
+from os import listdir, remove
+from os.path import isfile, isdir, join
+from face_engine import load_engine
 
 current_client_dict = {}
+chatroom_client_dict = {}
+ttt_wait_dict = {}
 
 
 def get_current_clients():
     global current_client_dict
     return current_client_dict
+
+
+
+def getFilePath(mypath):
+    files = listdir(mypath) 
+    file_list = []
+    for f in files:
+        fullpath = join(mypath, f)
+        if isfile(fullpath):
+            file_list.append(fullpath)
+        elif isdir(fullpath):
+            for f2 in getFilePath(fullpath):
+                file_list.append(f2)
+    return file_list
 
 
 
@@ -27,8 +46,24 @@ def send_img(connl, img_path):
 
 
 
+def recv_img(connl, save_path):
+    imgFile = open(save_path, 'wb')  
+    while True:
+        imgData = connl.recv(1024)
+        if imgData == "transmit end".encode("utf-8"):
+            break  
+        imgFile.write(imgData)
+    imgFile.close()
+
+
+
+
+
+
 def client_server(account, connl):
     global current_client_dict
+    global chatroom_client_dict
+    global ttt_wait_dict
     data = (MySQLFun.get_user_data()[account], account)
     connl.send(str(data).encode("utf-8"))
     grade = MySQLFun.get_user_data()[account][1]
@@ -38,22 +73,171 @@ def client_server(account, connl):
     while True:
         command = connl.recv(1024).decode("utf-8")
         if command == "00000":
+            
             del current_client_dict[account]
             connl.close()
             print("{} is disconnect" .format(account))
             break
+        
+        elif command == "00001":
+            
+            img_data_dict = MySQLFun.get_img_info()
+            connl.send(str(img_data_dict).encode("utf-8"))
+            
+        elif command == "00002":
+            
+            fileImg_name = connl.recv(1024).decode("utf-8")
+            img_store_list = getFilePath("face_img_store")
+            check_img_store_list = []
+            for name in img_store_list:
+                check_img_store_list.append(name[15:])
+            if fileImg_name not in check_img_store_list:
+                connl.send("can send img".encode("utf-8"))
+                file_img_path = "face_img_store\\"+fileImg_name
+                recv_img(connl, file_img_path)   
+                engine = load_engine("face_feature.p")#!!!
+                boxes, names = engine.make_prediction(file_img_path)
+                boxes = list(boxes)
+                for i in range(len(boxes)):
+                    boxes[i] = list(boxes[i])
+                names = names[0]
+                nowtime = MySQLFun.getTime()
+                img_face_data = (boxes, names, fileImg_name, nowtime, nowtime+"Untitled")
+                connl.send(str(img_face_data).encode("utf-8"))
+                MySQLFun.insert_to_img_info(names, fileImg_name, nowtime, nowtime+"Untitled")
+                img_data_dict = MySQLFun.get_img_info()
+                connl.send(str(img_data_dict).encode("utf-8"))     
+            else:
+                connl.send("img is exist".encode("utf-8"))
+        
+        elif command == "00003":
+            
+            nick_name = connl.recv(1024).decode("utf-8")
+            img_data_dict = MySQLFun.get_img_info()
+            connl.send(img_data_dict[nick_name][1].encode("utf-8"))
+            send_img(connl, "face_img_store\\" + img_data_dict[nick_name][1])
+            engine = load_engine("face_feature.p")
+            boxes, names = engine.make_prediction("face_img_store\\" + img_data_dict[nick_name][1])#!!!
+            boxes = list(boxes)
+            for i in range(len(boxes)):
+                boxes[i] = list(boxes[i])
+            names = names[0]
+            img_face_data2 = (boxes, names, img_data_dict[nick_name][1], img_data_dict[nick_name][2], nick_name)
+            connl.send(str(img_face_data2).encode("utf-8"))
+        
+        elif command == "00004":
+            
+            new_name = connl.recv(1024).decode("utf-8")
+            old_name = connl.recv(1024).decode("utf-8")
+            check_dict = MySQLFun.get_img_info()
+            if old_name in check_dict:
+                MySQLFun.update_img_info_Nick_name(new_name, old_name)
+                connl.send("config".encode("utf-8"))
+                new_img_data_dict = MySQLFun.get_img_info()
+                connl.send(str(new_img_data_dict).encode("utf-8"))
+            else:
+                connl.send("same in db".encode("utf-8"))
+        
+        elif command == "00005":
+            
+            nick_name = connl.recv(1024).decode("utf-8")
+            img_data_dict = MySQLFun.get_img_info()
+            remove("face_img_store\\" + img_data_dict[nick_name][1])
+            MySQLFun.delete_img_info(nick_name)
+            new_img_data_dict = MySQLFun.get_img_info()
+            connl.send(str(new_img_data_dict).encode("utf-8"))
+        
+        elif command == "00006":
+            
+            for client in chatroom_client_dict:
+                chatroom_client_dict[client].send("{} add the chatroom...".format(account).encode("utf-8"))
+            chatroom_client_dict[account] = connl
+            while True:
+                msg = connl.recv(1024).decode("utf-8")
+                if msg == "00000exit":
+                    connl.send("00000exit".encode("utf-8"))
+                    del chatroom_client_dict[account]
+                    for client in chatroom_client_dict:
+                        chatroom_client_dict[client].send("{} exit the chatroom...".format(account).encode("utf-8"))
+                    break
+                else:
+                    for client in chatroom_client_dict:
+                        chatroom_client_dict[client].send("{}: {}".format(account, msg).encode("utf-8"))
+                    MySQLFun.inser_to_chatroom_record(account, msg)
+        
+        elif command == "00007":
+            
+            phone_list = eval(connl.recv(1024).decode("utf-8"))
+            for phone in phone_list:
+                MySQLFun.insert_to_phone_record(phone)
+        
+        elif command == "00008":
+            
+            password_data = eval(connl.recv(1024).decode("utf-8"))
+            password = password_data[0]
+            new_password = password_data[1]
+            password = MySQLFun.hash256Encode(password)
+            new_password = MySQLFun.hash256Encode(new_password)
+            check_pw = MySQLFun.get_account_password()
+            if check_pw[account] == password:
+                connl.send("change password".encode("utf-8"))
+                MySQLFun.update_account_password(new_password, account)
+            else:
+                connl.send("error password".encode("utf-8"))
+        
+        elif command == "00009":
+            
+            user_data = MySQLFun.get_user_data()
+            user_name_list = []
+            for name in user_data:
+                user_name_list.append(name)
+            connl.send(str(user_name_list).encode("utf-8"))
+        
+        elif command == "00010":
+            
+            user_name_dict = MySQLFun.get_user_data()
+            user_name = connl.recv(1024).decode("utf-8")
+            if user_name in user_name_dict:
+                s_data = (user_name, user_name_dict[user_name])
+                connl.send(str(s_data).encode("utf-8"))
+            else:
+                connl.send("no name in db".encode("utf-8"))
+        
+        elif command == "00011":
+            
+            set_user_data = eval(connl.recv(1024).decode("utf-8"))
+            if set_user_data[0] in MySQLFun.get_user_data():
+                try:
+                    content = int(set_user_data[2])
+                    MySQLFun.update_user_data(set_user_data[1], content, set_user_data[0])
+                    connl.send("change successfully".encode("utf-8"))
+                except ValueError:
+                    connl.send("input error".encode("utf-8"))
+            else:
+                connl.send("name not in db".encode("utf-8"))
+        
+        elif command == "00012":
+            
+            del_account = connl.recv(1024).decode("utf-8")
+            if del_account in MySQLFun.get_user_data():
+                MySQLFun.delete_user_data_account(del_account)
+                MySQLFun.delete_account_password_account(del_account)
+                connl.send("delete successfully".encode("utf-8"))
+            else:
+                connl.send("name3 not in db".encode("utf-8"))
+            
 
+                
 
-
-
-
+            
+                
+        
 
 def check_password(connl, addr):
     global current_client_dict
     error_count = 1
     while True:
         user_data = connl.recv(1024).decode("utf-8")
-        print(user_data)
         if user_data == "00000":
             connl.close()
             print("{} is disconnect" .format(addr))
